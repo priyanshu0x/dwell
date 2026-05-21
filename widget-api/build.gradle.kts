ebcdef2c40a35a201a84524783d25b8f725f2e91
Priyanshu1999 <parmody1@gmail.com>
2026-05-21 23:58:06 +0530

    Wire widgetLayouts using GridRect from widget-api

--- widget-api/build.gradle.kts
+++ widget-api/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.multiplatform)
     alias(libs.plugins.compose.compiler)
     alias(libs.plugins.compose)
+    alias(libs.plugins.kotlinx.serialization)
 }
 
 kotlin {
