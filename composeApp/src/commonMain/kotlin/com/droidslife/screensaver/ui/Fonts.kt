4dfdb12f1b30b1e6453d5f3202e392e846be62a0
Priyanshu1999 <parmody1@gmail.com>
2026-05-21 23:56:20 +0530

    Wire Inter Tight + JetBrains Mono font families

---
+++ composeApp/src/commonMain/kotlin/com/droidslife/screensaver/ui/Fonts.kt
@@ -0,0 +1,38 @@
+package com.droidslife.screensaver.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import org.jetbrains.compose.resources.Font
+import screen_saver_app.composeapp.generated.resources.InterTight_Bold
+import screen_saver_app.composeapp.generated.resources.InterTight_ExtraLight
+import screen_saver_app.composeapp.generated.resources.InterTight_Light
+import screen_saver_app.composeapp.generated.resources.InterTight_Medium
+import screen_saver_app.composeapp.generated.resources.InterTight_Regular
+import screen_saver_app.composeapp.generated.resources.InterTight_SemiBold
+import screen_saver_app.composeapp.generated.resources.JetBrainsMono_ExtraLight
+import screen_saver_app.composeapp.generated.resources.JetBrainsMono_Light
+import screen_saver_app.composeapp.generated.resources.JetBrainsMono_Medium
+import screen_saver_app.composeapp.generated.resources.JetBrainsMono_Regular
+import screen_saver_app.composeapp.generated.resources.Res
+
+/** Bundled font families. Loaded via Compose Resources. */
+object DwellFonts {
+    @Composable
+    fun interTight(): FontFamily = FontFamily(
+        Font(Res.font.InterTight_ExtraLight, FontWeight.ExtraLight),
+        Font(Res.font.InterTight_Light, FontWeight.Light),
+        Font(Res.font.InterTight_Regular, FontWeight.Normal),
+        Font(Res.font.InterTight_Medium, FontWeight.Medium),
+        Font(Res.font.InterTight_SemiBold, FontWeight.SemiBold),
+        Font(Res.font.InterTight_Bold, FontWeight.Bold),
+    )
+
+    @Composable
+    fun jetBrainsMono(): FontFamily = FontFamily(
+        Font(Res.font.JetBrainsMono_ExtraLight, FontWeight.ExtraLight),
+        Font(Res.font.JetBrainsMono_Light, FontWeight.Light),
+        Font(Res.font.JetBrainsMono_Regular, FontWeight.Normal),
+        Font(Res.font.JetBrainsMono_Medium, FontWeight.Medium),
+    )
+}
